a9ec6c61b7c34143a811aeedeea0e4034203453e
Aditya <adityasinha@appscrip.co>
2024-05-17 00:02:23 +0530

    feat: initial commit

--- library/mongodb.js
+++ library/mongodb.js
@@ -3,5 +3,5 @@ const { infoLogger , erroLogger} = require('../utils/logger')
 
 mongoose.set('strictQuery', true)
 mongoose.connect(process.env.MONGODB_URL)
-.then(()=> infoLogger.info("Connected to database successfully"))
+.then(()=> infoLogger.info("Connected to  database successfully"))
 .catch(err => erroLogger.error(err))
